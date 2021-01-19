57487c82ce4a715cd423ebd7954923ed2ef45053
JuGiolo <juligiolo@gmail.com>
2021-01-19 20:45:31 +0000

    Learning

--- FirstFile.py
+++ FirstFile.py
@@ -1,1 +1,16 @@
+# this is a comment
 print('hello world')
+print('comment is not printed')
+print('hello Python')
+
+# print("This shouldn't print")
+print("this should print")
+
+print('hello','Juliana','Giolo')
+"""
+this add comments
+in more than one line
+"""
+print('Hello,')
+print('Juliana Giolo')
+print('Hello','Giolo')
