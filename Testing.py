f52ad2d4c789e00e14febaf20cebdaaefc566209
JuGiolo <juligiolo@gmail.com>
2021-01-19 23:02:10 +0000

    My first attempt alone

---
+++ Testing.py
@@ -0,0 +1,12 @@
+name = "Juliana Giolo"
+print("Greetings")
+print("by")
+print(name)
+print('\nTo Do list')
+to_do_list=["Run","Team meeting","Huddle","Shop","Global Sync","Lunch"]
+print(to_do_list[3])
+print(to_do_list[2])
+print(to_do_list[5])
+print(to_do_list[1])
+print(to_do_list[4])
+print(to_do_list[0])
