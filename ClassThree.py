83e57a0485c9e39789e0d5fc8defdc06d2100b7d
JuGiolo <juligiolo@gmail.com>
2021-01-31 23:31:48 +0000

    trying nested statement

--- ClassThree.py
+++ ClassThree.py
@@ -1,18 +1,18 @@
-"""
+
 
 sum = 5 + 3
 sum_2 = 2 + sum 
 sum_3 = sum + sum_2
 
-# my_list = (1,2,3,4)
-# my_list_2 = (5,6,7,8)
-# my_list_3 = my_list + my_list_2
+my_list = (1,2,3,4)
+my_list_2 = (5,6,7,8)
+my_list_3 = my_list + my_list_2
 
 my_list_4 = (sum, sum_2, sum_3, "First", "next", "last")
 
 
 print(type(sum))
-Use the command above to specify the type of language)
+# Use the command above to specify the type of language)
 
 print(sum + sum_2)
 
@@ -35,8 +35,7 @@ print(result) # Prints: True
 print(3 > 5) # Prints: False; since 3 is NOT greater than 5
 
 print(5 > 5) # Prints: False; since 5 is NOT greater than 5 (they are equal)
-"""
-"""
+
 x = 2 # Setting up the x variable
 
 if x < 3:
@@ -65,7 +64,7 @@ else:
     x -= 1 # This will run if x is not less than 3
 
 print(x) # Since this is on a lower indentation level, this code will run regardless
-"""
+
 
 user_value = int(input("Enter a number between 0 and 5: "))
 
--- Testing0.py
+++ Testing0.py
@@ -1,7 +1,8 @@
-user_value = int(input("Are you a student? Yes = 1  No = 2. "))
-if user_value == 1:
-    print("What is your name?")
-elif user_value == 2:
-    print("How old are you")
+user_value = (input("Are you a woman? Yes or No > "))
+if user_value == 'Yes':
+    print("Do you have a degree? Yes = 1  No = 2. >")
+elif user_value == 'No':
+    print("This is a research for women only. \nThank you for your time!")
+
 else: # If value is not yes or no
-    print("This is a Yes or No question")
+    print("Please, note that this is a Yes or No question.\nThank you")
