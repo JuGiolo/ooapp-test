a4a74d9c153b9b7c511b891c035346eb574bb9f9
JuGiolo <juligiolo@gmail.com>
2021-02-02 22:53:44 +0000

    need to review the exercises

--- Testing0.py
+++ Testing0.py
@@ -1,6 +1,7 @@
 user_value = (input("Are you a woman? Yes or No > "))
 if user_value == 'Yes':
     print("Do you have a degree? Yes = 1  No = 2. >")
+
 elif user_value == 'No':
     print("This is a research for women only. \nThank you for your time!")
 
